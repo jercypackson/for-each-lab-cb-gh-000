889abf7f86481c7c03b63822ba607fc1b30a5a46
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 18:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function iterativeLog(array){
   array.forEach((elem, idx) => console.log(`${idx}: ${elem}`));
 }
+
+function iterate(callback){
+  var array = [1,2,3];
+  array.forEach(callback);
+  return array;
+}
